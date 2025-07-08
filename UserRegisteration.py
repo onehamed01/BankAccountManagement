2f78babd242616f95229bbf25d37aa1179c5bbef
onehamed01 <onehamed01@Gmail.com>
2025-07-08 08:31:17 +0100

    add save_data method to save Dict into Json

--- UserRegisteration.py
+++ UserRegisteration.py
@@ -8,16 +8,27 @@ class RegisterAccount:
         self.postcode = postcode
         self.init_amount = init_amount
 
-    def to_dict(self):
-        db = {
-            "fullname":self.fullname,
-            "age":self.age,
-            "contact":self.contact,
-            "postcode":self.postcode,
-            "init_amount":self.init_amount
-        }
-        return db
+    def save_data(self):
+        db = []
+        try:
+            with open("db.json", 'r', encoding='utf-8') as f:
+                db = json.load(f)
+        except Exception as e:
+            print(f"Error: {e}. File has Created")
 
-
+        db.append({
+            "Fullname":self.fullname,
+            "Age":self.age,
+            "Contact":self.fullname,
+            "Postcode":self.postcode,
+            "Amount":self.init_amount,
+        })
+        
+        with open("db.json", 'w', encoding='utf-8') as f:
+            json.dump(db, f, ensure_ascii=False, indent=4)
 
     
+hami = RegisterAccount("Hamed Mohammad vand", 23, 7733009227, "NE4 8QH", 120)
+sophia = RegisterAccount("Sophia Marlin", 27, 7733009389, "NE6 1BH", 80)
+hami.save_data()
+sophia.save_data()
